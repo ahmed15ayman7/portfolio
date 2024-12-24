ba4a3996fe295f50d0cdd438675ed2096c97f782
ahmed15ayman7 <111033436+ahmed15ayman7@users.noreply.github.com>
2024-12-24 14:47:25 +0200

    Update FloatingNavbar.tsx

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -76,7 +76,9 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center  flex space-x-1 dark:hover:text-neutral-300 hover:text-neutral-500"
+              `relative dark:text-neutral-50 items-center  flex space-x-1 dark:hover:text-neutral-300 hover:text-neutral-500 ${
+                navItem.name === "Testimonials" ? "max-sm:hidden" : ""
+              }`
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
